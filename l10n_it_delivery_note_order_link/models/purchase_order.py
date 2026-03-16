c01beb6a443c72c00c8426141944f1be890b5f53
Alessio <segreteria@clickode.it>
2026-03-16 12:44:35 +0100

    Applied PR 5080.

--- l10n_it_delivery_note_order_link/models/purchase_order.py
+++ l10n_it_delivery_note_order_link/models/purchase_order.py
@@ -2,6 +2,7 @@
 # @author: Matteo Bilotta <mbilotta@linkeurope.it>
 
 from odoo import fields, models
+from odoo.fields import Domain
 
 
 class PurchaseOrder(models.Model):
@@ -29,13 +30,13 @@ class PurchaseOrder(models.Model):
         action.update(kwargs)
 
         if len(delivery_notes) > 1:
-            action["domain"] = [("id", "in", delivery_notes.ids)]
+            action["domain"] = Domain("id", "in", delivery_notes.ids)
 
         elif len(delivery_notes) == 1:
             action["views"] = [
                 (
                     self.env.ref(
-                        "l10n_it_delivery_note." "stock_delivery_note_form_view"
+                        "l10n_it_delivery_note.stock_delivery_note_form_view"
                     ).id,
                     "form",
                 )
